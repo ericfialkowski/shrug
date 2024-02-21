a2970f706a261578246d3d0f225a0045773986be
Eric Fialkowski <eric.fialkowski@gmail.com>
2024-02-20 20:22:25 -0700

    clipboard copying and escaped output

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shrug
 
 go 1.17
+
+require github.com/atotto/clipboard v0.1.4
--- main.go
+++ main.go
@@ -3,20 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/atotto/clipboard"
 )
 
 const shrug = "¯\\_(ツ)_/¯"
+const escapedShrug = "¯\\\\_(ツ)_/¯"
 
 var newline = flag.Bool("n", false, "Output with New Line")
+var escape = flag.Bool("e", false, "Escape the output")
+var clip = flag.Bool("c", true, "Copy to clipboard")
+var stdOut = flag.Bool("s", true, "Print output, if false copy to clipboard is forced true")
 
 func init() {
 	flag.Parse()
 }
 
-func main() {
+func gen() string {
+	ec := ""
 	if *newline {
-		fmt.Println(shrug)
-	} else {
-		fmt.Print(shrug)
+		ec = "\n"
+	}
+	s := shrug
+	if *escape {
+		s = escapedShrug
+	}
+	return fmt.Sprintf("%s%s", s, ec)
+}
+
+func main() {
+	s := gen()
+	if *clip || !*stdOut {
+		_ = clipboard.WriteAll(s)
+	}
+	if *stdOut {
+		fmt.Print(s)
 	}
 }
